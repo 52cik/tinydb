bf3c0a18fd2679186d9325131f0332e9997e7b77
楼教主 <cik520@qq.com>
2016-07-18 15:35:52 +0800

    添加测试代码

--- test/index.js
+++ test/index.js
@@ -6,31 +6,33 @@ const tinydb = require('..')
 
 describe('测试用例:', _ => {
 
-    it('基础测试', () => {
-        let db = new tinydb
+  it('基础测试', () => {
+    let db = new tinydb
 
-        db.set('key', 'val')
-        db.get('key').should.equal('val')
+    db.set('key', 'val')
+    db.get('key').should.equal('val')
 
-        db.del('key')
-        should(db.get('key')).be.exactly(undefined)
-    })
+    db.del('key')
+    should(db.get('key')).be.exactly(undefined)
 
-    it('多次实例化测试', () => {
-        new tinydb()
-            .set('key', 'val')
-            .get('key').should.equal('val')
+    db.clear()
+  })
 
-        new tinydb()
-            .set('key', 'newval')
-            .get('key').should.equal('newval')
-    })
+  it('多次实例化测试', () => {
+    new tinydb()
+      .set('key', 'val')
+      .get('key').should.equal('val')
 
-    it('复杂数据测试', () => {
-        new tinydb()
-            .set('obj', {a: {b: {c: 1}, d: 2}})
-            .set('obj', {a: {b: {c: 5}}})
-            .get('obj').should.be.eql({a: {b: {c: 5}}})
-    })
+    new tinydb()
+      .set('key', 'newval')
+      .get('key').should.equal('newval')
+  })
+
+  it('复杂数据测试', () => {
+    new tinydb()
+      .set('obj', {a: {b: {c: 1}, d: 2}})
+      .set('obj', {a: {b: {c: 5}}})
+      .get('obj').should.be.eql({a: {b: {c: 5}}})
+  })
 
 })
